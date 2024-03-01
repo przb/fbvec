dedbc973c5d973c6c5a4e2652e23dfbbfbda7add
Ben Przybyszewski <przybyszewski.benjamin@gmail.com>
2024-03-01 17:10:04 -0600

    refactor rawvec for intoiter and fbvec

--- src/fbvec.rs
+++ src/fbvec.rs
@@ -60,8 +60,7 @@ impl<T> Drop for RawFbVec<T> {
     }
 }
 pub struct FbVec<T> {
-    ptr: NonNull<T>,
-    cap: usize,
+    buf: RawFbVec<T>,
     len: usize,
 }
 
@@ -69,49 +68,24 @@ unsafe impl<T: Send> Send for FbVec<T> {}
 unsafe impl<T: Sync> Sync for FbVec<T> {}
 
 impl<T> FbVec<T> {
+    fn ptr(&self) -> *mut T {
+        self.buf.ptr.as_ptr()
+    }
+    fn cap(&self) -> usize {
+        self.buf.cap
+    }
     pub fn new() -> Self {
-        assert!(mem::size_of::<T>() != 0, "Not ready for ZSTs");
-        Self {
-            ptr: NonNull::dangling(),
-            cap: 0,
+        FbVec {
+            buf: RawFbVec::new(),
             len: 0,
         }
     }
-    fn grow(&mut self) {
-        let (new_cap, new_layout) = if self.cap == 0 {
-            (1, Layout::array::<T>(1).unwrap())
-        } else {
-            let new_cap = GROWTH_RATE * self.cap;
-
-            let new_layout = Layout::array::<T>(new_cap).unwrap();
-            (new_cap, new_layout)
-        };
-
-        assert!(
-            new_layout.size() <= isize::MAX as usize,
-            "allocation too large"
-        );
-
-        let new_ptr = if self.cap == 0 {
-            unsafe { alloc::alloc(new_layout) }
-        } else {
-            let old_layout = Layout::array::<T>(self.cap).unwrap();
-            let old_ptr = self.ptr.as_ptr() as *mut u8;
-            unsafe { alloc::realloc(old_ptr, old_layout, new_layout.size()) }
-        };
-
-        self.ptr = match NonNull::new(new_ptr as *mut T) {
-            Some(p) => p,
-            None => alloc::handle_alloc_error(new_layout),
-        };
-        self.cap = new_cap;
-    }
     pub fn push(&mut self, elem: T) {
-        if self.len == self.cap {
-            self.grow();
+        if self.len == self.cap() {
+            self.buf.grow();
         }
         unsafe {
-            ptr::write(self.ptr.as_ptr().add(self.len), elem);
+            ptr::write(self.ptr().add(self.len()), elem);
         }
         self.len += 1;
     }
@@ -120,21 +94,21 @@ impl<T> FbVec<T> {
             None
         } else {
             self.len -= 1;
-            unsafe { Some(ptr::read(self.ptr.as_ptr().add(self.len))) }
+            unsafe { Some(ptr::read(self.ptr().add(self.len))) }
         }
     }
     pub fn insert(&mut self, index: usize, elem: T) {
         assert!(index <= self.len(), "Index out of bounds");
-        if self.len == self.cap {
-            self.grow();
+        if self.len == self.cap() {
+            self.buf.grow();
         }
         unsafe {
             ptr::copy(
-                self.ptr.as_ptr().add(index),
-                self.ptr.as_ptr().add(index + 1),
+                self.ptr().add(index),
+                self.ptr().add(index + 1),
                 self.len - index,
             );
-            ptr::write(self.ptr.as_ptr().add(index), elem)
+            ptr::write(self.ptr().add(index), elem)
         }
         self.len += 1;
     }
@@ -142,10 +116,10 @@ impl<T> FbVec<T> {
         assert!(index < self.len, "index out of bounds");
         unsafe {
             self.len -= 1;
-            let result = ptr::read(self.ptr.as_ptr().add(index));
+            let result = ptr::read(self.ptr().add(index));
             ptr::copy(
-                self.ptr.as_ptr().add(index + 1),
-                self.ptr.as_ptr().add(index),
+                self.ptr().add(index + 1),
+                self.ptr().add(index),
                 self.len - index,
             );
             result
@@ -155,11 +129,7 @@ impl<T> FbVec<T> {
 
 impl<T> Drop for FbVec<T> {
     fn drop(&mut self) {
-        if self.cap != 0 {
-            while let Some(_) = self.pop() {}
-            let layout = Layout::array::<T>(self.cap).unwrap();
-            unsafe { alloc::dealloc(self.ptr.as_ptr() as *mut u8, layout) }
-        }
+        while let Some(_) = self.pop() {}
     }
 }
 
@@ -167,19 +137,18 @@ impl<T> Deref for FbVec<T> {
     type Target = [T];
 
     fn deref(&self) -> &Self::Target {
-        unsafe { std::slice::from_raw_parts(self.ptr.as_ptr(), self.len) }
+        unsafe { std::slice::from_raw_parts(self.ptr(), self.len) }
     }
 }
 
 impl<T> DerefMut for FbVec<T> {
     fn deref_mut(&mut self) -> &mut Self::Target {
-        unsafe { std::slice::from_raw_parts_mut(self.ptr.as_ptr(), self.len) }
+        unsafe { std::slice::from_raw_parts_mut(self.ptr(), self.len) }
     }
 }
 
 pub struct IntoIter<T> {
-    buf: NonNull<T>,
-    cap: usize,
+    _buf: RawFbVec<T>,
     start: *const T,
     end: *const T,
 }
@@ -190,21 +159,17 @@ impl<T> IntoIterator for FbVec<T> {
     type IntoIter = IntoIter<T>;
 
     fn into_iter(self) -> Self::IntoIter {
-        let vec = ManuallyDrop::new(self);
-
-        let ptr = vec.ptr;
-        let cap = vec.cap;
-        let len = vec.len;
-
+        let buf = unsafe { ptr::read(&self.buf) };
+        let len = self.len;
+        mem::forget(self);
         IntoIter {
-            buf: ptr,
-            cap,
-            start: ptr.as_ptr(),
-            end: if cap == 0 {
-                ptr.as_ptr()
+            start: buf.ptr.as_ptr(),
+            end: if buf.cap == 0 {
+                buf.ptr.as_ptr()
             } else {
-                unsafe { ptr.as_ptr().add(len) }
+                unsafe { buf.ptr.as_ptr().add(len) }
             },
+            _buf: buf,
         }
     }
 }
@@ -241,12 +206,6 @@ impl<T> DoubleEndedIterator for IntoIter<T> {
 }
 impl<T> Drop for IntoIter<T> {
     fn drop(&mut self) {
-        if self.cap == 0 {
-            for _ in &mut *self {}
-            let layout = Layout::array::<T>(self.cap).unwrap();
-            unsafe {
-                alloc::dealloc(self.buf.as_ptr() as *mut u8, layout);
-            }
-        }
+        for _ in &mut *self {}
     }
 }
